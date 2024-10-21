098d090923ba3f0ab8496fcefca55c5e12e436a0
Daniel Murray-Chadfield <damu0222@netcracker.com>
2024-10-21 21:28:38 +0100

    Fix long addition for many-digit numbers

--- long_babbage.py
+++ long_babbage.py
@@ -1,4 +1,5 @@
 import babbage as b
+max_digits = 40
 
 def pad_with_zeros(list, desired_length):
     list += [0] * (desired_length - len(list))
@@ -25,19 +26,13 @@ def format_numbers(add, acc):
     return add_list, acc_list, most_digits + 1
 
 def perform_long_addition(add, acc):
-    print(f"\nADDING {add} TO {acc}")
     add_list, acc_list, most_digits = format_numbers(add, acc)
-    carry_list = [0] * max_digits
-    print()
+    carry_list = [0] * (max_digits + 1)
     for i in range(most_digits):
-        b.display_values(add_list[i], acc_list[i], carry_list[i])
         add_list[i], acc_list[i], carry_list[i + 1] = b.perform_babbage_addition(add_list[i], acc_list[i], carry_list[i])
         carry_list[i] = 0
-        b.display_values(add_list[i], acc_list[i], carry_list[i])
-        print()
 
-    print()
-    b.display_values(to_number(add_list), to_number(acc_list), to_number(carry_list))
+    return to_number(add_list), to_number(acc_list), to_number(carry_list)
 
 
 def perform_long_subtraction(sub, acc):
@@ -56,7 +51,6 @@ def perform_long_subtraction(sub, acc):
     b.display_values(to_number(sub_list), to_number(acc_list), to_number(carry_list))
 
 if __name__ == "__main__":
-    max_digits = 40
 
     try:
         print("Addend:")
