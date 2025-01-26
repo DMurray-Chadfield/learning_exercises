eb45c7db7d34be76dc60174668da3a160a683880
Dan Murray-Chadfield <dan.chadfield@gmail.com>
2025-01-26 16:15:55 +0000

    Start code to number project

---
+++ general/java_playground/src/StringToNumber.java
@@ -0,0 +1,11 @@
+public class StringToNumber {
+    
+    public static void main(String[] args) {
+
+    }
+
+    private Integer convertStringToNumber(String input) {
+        String[] words = input.split(" ");
+
+    }
+}
