e36cfdde8f3f2d734279351b975f4b2596e381aa
Dan Murray-Chadfield <dan.chadfield@gmail.com>
2025-02-14 16:19:10 +0000

    Add unneccesary singleton scope, and autowired setter injection

--- pluralsight/spring_5_fundamentals/src/main/java/AppConfig.java
+++ pluralsight/spring_5_fundamentals/src/main/java/AppConfig.java
@@ -2,18 +2,25 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         // Using constructor injection
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+
         // Using setter injection
         //service.setRepository(getSpeakerRepository());
+
+        // Using setting injection with autowiring
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
         return service;
     }
 
