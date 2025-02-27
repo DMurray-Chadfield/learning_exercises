1e2964802e4b67c92283d268d3106d9a39d10c93
Dan Murray-Chadfield <dan.chadfield@gmail.com>
2025-02-27 14:17:15 +0000

    Add calendar using FactoryBean

--- pluralsight/spring_5_fundamentals/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ pluralsight/spring_5_fundamentals/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+    @Autowired
+    private Calendar cal;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -17,6 +22,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setLastName("MC");
 
         speakers.add(speaker);
+
+        System.out.println("cal: " + cal.getTime());
 
         return speakers;
     }
